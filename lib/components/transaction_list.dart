d047cc78961aa82c9231945c59ae7e0193dcdd3a
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-01-12 05:52:53 -0300

    107. Usando ListView

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,52 +10,52 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((tr) {
-            return Card(
-              child: Row(
-                children: [
-                  Container(
-                    margin:
-                        const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.purple, width: 2),
-                      borderRadius: const BorderRadius.all(Radius.circular(5)),
-                    ),
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      // "R\$ ${tr.value.toStringAsFixed(2)}",
-                      NumberFormat('R\$ #.00', 'pt-BR').format(tr.value),
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.purple,
-                          fontSize: 20),
-                    ),
+      child: ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (ctx, index) {
+          final tr = transactions[index];
+          return Card(
+            child: Row(
+              children: [
+                Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.purple, width: 2),
+                    borderRadius: const BorderRadius.all(Radius.circular(5)),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tr.title,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    // "R\$ ${tr.value.toStringAsFixed(2)}",
+                    NumberFormat('R\$ #.00', 'pt-BR').format(tr.value),
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.purple,
+                        fontSize: 20),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      tr.title,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
                       ),
-                      Text(
-                        DateFormat('d MMM y').format(tr.date),
-                        style: const TextStyle(
-                          color: Colors.grey,
-                        ),
+                    ),
+                    Text(
+                      DateFormat('d MMM y').format(tr.date),
+                      style: const TextStyle(
+                        color: Colors.grey,
                       ),
-                    ],
-                  )
-                ],
-              ),
-            );
-          }).toList(),
-        ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
       ),
     );
   }
