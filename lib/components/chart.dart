adb164d2ff20f3e72227edea5a1e2b1dcf907fa1
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-01-16 06:31:30 -0300

    123. Chart Widget #04

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/components/chart_bar.dart';
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -37,8 +38,11 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: const EdgeInsets.all(20),
       child: Row(
-        children:  groupedTransctions.map((tr) {
-          return Text('${tr['day']}: ${tr['value']}');
+        children: groupedTransctions.map((tr) {
+          return ChartBar(
+            label: tr['day'] as String,
+            value: tr['value'] as double,
+          );
         }).toList(),
       ),
     );
