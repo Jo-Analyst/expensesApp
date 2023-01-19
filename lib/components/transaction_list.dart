d67d4fe2d33f01b291e0aad8888fdbb927101f7e
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-01-19 06:42:17 -0300

    134. Excluindo Transação

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -3,8 +3,9 @@ import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
+  final void Function(String id) onRemove;
   final List<Transaction> transactions;
-  const TransactionList(this.transactions, {Key? key}) : super(key: key);
+  const TransactionList(this.transactions, this.onRemove, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -34,10 +35,8 @@ class TransactionList extends StatelessWidget {
                 final tr = transactions[index];
                 return Card(
                   elevation: 5,
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5
-                  ),
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
                   child: ListTile(
                     leading: CircleAvatar(
                       backgroundColor: Colors.purple,
@@ -45,8 +44,8 @@ class TransactionList extends StatelessWidget {
                       child: Padding(
                         padding: const EdgeInsets.all(6),
                         child: FittedBox(
-                          child: Text(
-                              NumberFormat('R\$ #.00', 'pt-BR').format(tr.value)),
+                          child: Text(NumberFormat('R\$ #.00', 'pt-BR')
+                              .format(tr.value)),
                         ),
                       ),
                     ),
@@ -56,6 +55,11 @@ class TransactionList extends StatelessWidget {
                     ),
                     subtitle: Text(
                       DateFormat("d MMM y").format(tr.date),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      color: Theme.of(context).errorColor,
+                      onPressed: () => onRemove(tr.id),
                     ),
                   ),
                 );
