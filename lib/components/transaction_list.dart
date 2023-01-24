374d9046be1dcf0e44392d4bbe11b37153a7fa90
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-01-24 05:48:51 -0300

    46. Modo Paisagem #02

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -5,27 +5,37 @@ import '../models/transaction.dart';
 class TransactionList extends StatelessWidget {
   final void Function(String id) onRemove;
   final List<Transaction> transactions;
-  const TransactionList(this.transactions, this.onRemove, {Key? key}) : super(key: key);
+  const TransactionList(this.transactions, this.onRemove, {Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? Column(
-            children: [
-              const SizedBox(height: 20),
-              Text(
-                "Nenhuma Transação cadastrada!",
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                height: 200,
-                child: Image.asset(
-                  'assets/images/waiting.png',
-                  fit: BoxFit.cover,
-                ),
-              )
-            ],
+        ? LayoutBuilder(
+            builder: (ctx, constraints) {
+              return Column(
+                children: [
+                  SizedBox(
+                    height: constraints.maxHeight * .05,
+                  ),
+                  SizedBox(
+                    height: constraints.maxHeight * .3,
+                    child: Text(
+                      "Nenhuma Transação cadastrada!",
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  ),
+                  SizedBox(height: constraints.maxHeight * .05),
+                  SizedBox(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ),
+                  )
+                ],
+              );
+            },
           )
         : ListView.builder(
             itemCount: transactions.length,
@@ -33,8 +43,7 @@ class TransactionList extends StatelessWidget {
               final tr = transactions[index];
               return Card(
                 elevation: 5,
-                margin:
-                    const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
                 child: ListTile(
                   leading: CircleAvatar(
                     backgroundColor: Colors.purple,
@@ -42,8 +51,8 @@ class TransactionList extends StatelessWidget {
                     child: Padding(
                       padding: const EdgeInsets.all(6),
                       child: FittedBox(
-                        child: Text(NumberFormat('R\$ #.00', 'pt-BR')
-                            .format(tr.value)),
+                        child: Text(
+                            NumberFormat('R\$ #.00', 'pt-BR').format(tr.value)),
                       ),
                     ),
                   ),
